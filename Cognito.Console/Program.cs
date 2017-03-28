839171a0380481abb865cc99d6e1e7ef8c09ecf2
Ro Se <robert.senktas@gmail.com>
2017-03-28 22:45:33 +0200

    Add simple code to check connection.

--- Cognito.Console/Program.cs
+++ Cognito.Console/Program.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
+using Amazon.Runtime.Internal.Auth;
 
 namespace Cognito.Console
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            var poolId = Environment.GetEnvironmentVariable("CognitoPoolId");
-            var clientId = Environment.GetEnvironmentVariable("CognitoClientId");
+            // Just for first time setup
+            //Environment.SetEnvironmentVariable("CognitoPoolId", "", EnvironmentVariableTarget.User);
+            //Environment.SetEnvironmentVariable("CognitoClientId", "", EnvironmentVariableTarget.User);
 
+            var poolId = Environment.GetEnvironmentVariable("CognitoPoolId",EnvironmentVariableTarget.User);
+            var clientId = Environment.GetEnvironmentVariable("CognitoClientId", EnvironmentVariableTarget.User);
+
+
+            using (var client = new AmazonCognitoIdentityProviderClient())
+            {
+                var response = client.DescribeUserPool(new DescribeUserPoolRequest() {UserPoolId = poolId});
+                System.Console.WriteLine(response.HttpStatusCode);
+            }
         }
     }
 }
